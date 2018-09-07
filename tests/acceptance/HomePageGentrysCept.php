6598359605ff9ca02b89b8e0d039f7f978f1a668
evg-belousov <evg.belousov@yahoo.com>
2018-09-07 17:46:17 +0300

    testing changes

--- tests/acceptance/HomePageGentrysCept.php
+++ tests/acceptance/HomePageGentrysCept.php
@@ -3,5 +3,4 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('perform actions and see result');
 
 $I->amOnPage('/');
-$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 $I->see('мальчики'); // Фраза или ее часть с главной страницы
